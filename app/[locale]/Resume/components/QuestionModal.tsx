105b1bc8a70b3ccdd38b889a206ac6f9e8183a2c
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2024-06-09 19:39:10 +0200

    Initial Implementation of Resume.

---
+++ app/[locale]/Resume/components/QuestionModal.tsx
@@ -0,0 +1,76 @@
+'use client';
+import {JobTypeEnum} from '@/src/data/Resume';
+import {Button, Modal} from 'flowbite-react';
+import {useTranslations} from 'next-intl';
+import {ReactNode} from 'react';
+
+let jobeNum: JobTypeEnum = JobTypeEnum.None;
+
+export function QuestionModal(props: {
+  openModalStatus: boolean;
+  closeModalCallback: (jobeNum: JobTypeEnum) => void;
+}): ReactNode {
+  const t = useTranslations('Resume.QuestionModal');
+  return (
+    <Modal
+      show={props.openModalStatus}
+      onClose={() => props.closeModalCallback}
+    >
+      <Modal.Header>Small modal</Modal.Header>
+      <Modal.Body>
+        <div className="space-y-6 p-6">
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Hi!, Thanks for getting interested in me, As I am multidisciplined
+            developer, I am obligated to get some resumes optimized for each
+            role,
+          </p>
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <Button.Group>
+              <Button
+                color="gray"
+                onClick={() => {
+                  jobeNum = JobTypeEnum.None;
+                }}
+              >
+                {t('all')}
+              </Button>
+              <Button
+                color="gray"
+                onClick={() => {
+                  jobeNum = JobTypeEnum.GameDev;
+                }}
+              >
+                Game Developer/Interative App/Virtual Production
+              </Button>
+              <Button
+                color="gray"
+                onClick={() => {
+                  jobeNum = JobTypeEnum.Web;
+                }}
+              >
+                {t('web')}
+              </Button>
+              <Button
+                color="gray"
+                onClick={() => {
+                  jobeNum = JobTypeEnum.Backend;
+                }}
+              >
+                {t('backend')}
+              </Button>
+            </Button.Group>
+          </p>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          onClick={() => {
+            props.closeModalCallback(jobeNum);
+          }}
+        >
+          I accept
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
---
+++ src/data/Resume.ts
@@ -0,0 +1,283 @@
+import {useTranslations} from 'next-intl';
+
+export enum JobTypeEnum {
+  None = 0,
+  GameDev = 1,
+  Backend = 2,
+  Web = 3,
+}
+
+class CommonMinimum {
+  Title = '';
+  StartDate = 0;
+  EndDate = 0;
+}
+
+class CommonMinimumWithLocation extends CommonMinimum {
+  Location = '';
+  Url = '';
+}
+
+export class JobsType extends CommonMinimum {
+  Description = '';
+  JobType = JobTypeEnum.None;
+  BulletPoints: Array<string> = [];
+  techStack: Array<string> = [];
+}
+
+export class CompanyType extends CommonMinimumWithLocation {
+  typeOfHire = '';
+  typeOfPresence = '';
+  Jobs: Array<JobsType> = [];
+}
+
+export class EducationType extends CommonMinimumWithLocation {
+  IssuerOrg = '';
+  EFramework = '';
+  originalTitle = '';
+  techStack: Array<string> = [];
+}
+
+export type ResumeType = {
+  Jobs: Array<CompanyType>;
+  Education: Array<EducationType>;
+  Certificate: Array<{
+    title: string;
+    type: string;
+    IssuerOrg: string;
+    date: number;
+  }>;
+  Talks: Array<{title: string; location: string; date: number}>;
+};
+
+export function ResumeContent(): ResumeType {
+  const t = useTranslations('Resume');
+  return {
+    Jobs: [
+      {
+        Title: 'Abance',
+        Location: t('Locations.Puerto'),
+        Url: '',
+        StartDate: Date.parse('2023/09/25'),
+        EndDate: Date.parse('2024/09/01'),
+        Jobs: [
+          {
+            Title: 'Typescript / React / Next.JS Developer',
+            Description: '',
+            JobType: JobTypeEnum.Web,
+            StartDate: Date.parse('2024/05/15'),
+            EndDate: Date.now(),
+            techStack: ['typescript', 'react', 'nextjs'],
+            BulletPoints: [],
+          },
+          {
+            Title: 'Wordpress Developer',
+            Description: '',
+            JobType: JobTypeEnum.Web,
+            StartDate: Date.parse('2024/03/10'),
+            EndDate: Date.now(),
+            techStack: ['oxygen', 'css'],
+            BulletPoints: [],
+          },
+          {
+            Title: 'Lead C# Software Developer',
+            Description: t('Jobs.Abance.csharpdev.Description'),
+            JobType: JobTypeEnum.Backend,
+            StartDate: Date.parse('2024/02/24'),
+            EndDate: Date.now(),
+            techStack: ['csharp'],
+            BulletPoints: [
+              'Excel Parsing (OpenXML)',
+              'HTMLScrapper',
+              '.NET Ecosystem Consultant ( architecture migration)',
+              'Testing Automatization Designer',
+            ],
+          },
+          {
+            Title: 'Lead Unreal Engine VR Developer',
+            Description: '',
+            StartDate: Date.parse('2023/09/25'),
+            EndDate: Date.parse('2024/02/24'),
+            JobType: JobTypeEnum.GameDev,
+            techStack: ['unreal'],
+            BulletPoints: [
+              'Networking',
+              'Builds Pipeline (Server Included)',
+              'Automatized Content Build Pipeline with QT tools and Multithreaded optimisations)',
+              'I/O Optimization (reduction of 10-15 Min to a few seconds On Load)',
+              'Python Automatization (Commandlet and In-Editor)',
+              'Accessibility Options',
+              'Custom Features',
+            ],
+          },
+        ],
+        typeOfHire: 'full',
+        typeOfPresence: 'hybrid',
+      },
+      {
+        Title: 'Itixo | ICT Capital s.r.o',
+        Location: t('Locations.Prague'),
+        Url: 'https://www.itixo.com/',
+        StartDate: Date.parse('2018/02/27'),
+        EndDate: Date.parse('2020/06/15'),
+        Jobs: [
+          {
+            Title: 'Backend Developer',
+
+            Description:
+              'Software Creation y maintenance in .NET Framework and migration tasks to .NET Standard/Core',
+            JobType: JobTypeEnum.Backend,
+            StartDate: Date.parse('2018/02/27'),
+            EndDate: Date.parse('2020/06/15'),
+            techStack: [
+              'csharp',
+              'sqlserver',
+              'azure',
+              'msTests',
+              'xunit',
+              'entity',
+            ],
+            BulletPoints: [
+              'Refactoring Tasks.',
+              'Creation and Design of Unit Tests for old and new projects',
+              'Creation of Software for Azure Platform',
+            ],
+          },
+        ],
+        typeOfHire: 'freelance',
+        typeOfPresence: 'remote',
+      },
+      {
+        Title: 'KnowledgeMill',
+        Location: t('Locations.London'),
+        Url: 'https://knowledgemill.com/',
+        StartDate: Date.parse('2017/07/27'),
+        EndDate: Date.parse('2017/12/04'),
+        Jobs: [
+          {
+            Title: 'WebApp Dev',
+            Description:
+              'Remade a Web Application originally made with Microsoft Silverlight to Node.js with REST API reverse engineering (because lack of documentation)',
+            JobType: JobTypeEnum.Web,
+            StartDate: Date.parse('2017/08/15'),
+            EndDate: Date.parse('2017/12/15'),
+            techStack: ['nodejs', 'bootstrap', 'exchangeapi', 'centos'],
+            BulletPoints: [
+              'Improving WebApp offering Microsoft Exchange Service Support and Integration like other newer Company’s products.',
+              'Redesign Front Page.',
+              'RPM Packing and init.d configuration',
+            ],
+          },
+          {
+            Title: 'Apprenticeship WebApp Dev',
+            Description: '',
+            JobType: JobTypeEnum.Web,
+            StartDate: Date.parse('2017/07/27'),
+            EndDate: Date.parse('2017/08/15'),
+            techStack: ['nodejs', 'bootstrap', 'exchangeapi', 'centos'],
+            BulletPoints: [],
+          },
+        ],
+        typeOfHire: 'full',
+        typeOfPresence: 'On-site',
+      },
+      {
+        Title: 'Viewnext',
+        Url: 'https://www.viewnext.com/',
+        Location: t('Locations.Caceres'),
+        StartDate: Date.parse('2017/03/01'),
+        EndDate: Date.parse('2017/06/01'),
+        Jobs: [
+          {
+            Title: 'Trainee Software Developer',
+            Description: '',
+            JobType: JobTypeEnum.Backend,
+            StartDate: Date.parse('2017/03/01'),
+            EndDate: Date.parse('2017/06/01'),
+            techStack: [],
+            BulletPoints: [],
+          },
+        ],
+        typeOfHire: 'full',
+        typeOfPresence: 'On-site',
+      },
+    ],
+    Education: [
+      {
+        Title: t('Education.DAM.Title'),
+        originalTitle:
+          'Técnico Superior en Desarrollo de Aplicaciones Multiplataforma',
+        EFramework: 'EQF5/ISCED5',
+        IssuerOrg: 'I.E.S Agora',
+        Location: t('Locations.Caceres'),
+        StartDate: Date.parse('2015'),
+        EndDate: Date.parse('2017'),
+        Url: 'https://www.todofp.es/dam/jcr:7655e32d-08a3-47a7-a479-ddb6f032c63e/n-tsdesarrolloaplicacionesmultiplataformaen-pdf.pdf',
+        techStack: ['java', 'csharp', 'sqlserver'],
+      },
+      {
+        Title: t('Education.SMR.Title'),
+        originalTitle: 'Técnico en Sistemas Microinformáticos y Redes',
+        EFramework: 'ISCED3',
+        IssuerOrg: 'Educatec',
+        Location: t('Locations.Caceres'),
+        StartDate: Date.parse('2012'),
+        EndDate: Date.parse('2014'),
+        Url: 'https://www.todofp.es/dam/jcr:3a2f6e25-6324-4ced-8c08-ca6f65927900/n-tsistemasmicroinformaticosredesen-pdf.pdf',
+        techStack: [],
+      },
+    ],
+    Certificate: [
+      {
+        title: 'Unreal Engine C++ Developer: Learn C++ and Make Video Games',
+        type: 'gamedev',
+        IssuerOrg: 'Udemy',
+        date: Date.parse('2019/05/01'),
+      },
+      {
+        title:
+          'CCNA Discovery 2 : Working at a Small-to-Medium Business or ISP',
+        type: 'network',
+        IssuerOrg: 'Cisco',
+        date: Date.parse('2013/06/01'),
+      },
+      {
+        title: 'Adobe Photoshop CS3 Course',
+        type: 'design',
+        IssuerOrg: 'Grupo System',
+        date: Date.parse('2013/06/01'),
+      },
+      {
+        title: 'PowerPoint 2010',
+        type: 'office',
+        IssuerOrg: 'Grupo System',
+        date: Date.parse('2013/06/01'),
+      },
+      {
+        title: 'Access 2010',
+        type: 'office',
+        IssuerOrg: 'Grupo System',
+        date: Date.parse('2013/06/01'),
+      },
+      {
+        title: 'CCNA Discovery 1 : Networking for Home and Small Businesses',
+        type: 'office',
+        IssuerOrg: 'Grupo System',
+        date: Date.parse('2013/02/01'),
+      },
+      {
+        title: 'OpenOffice Writer',
+        type: 'office',
+        IssuerOrg: 'Grupo System',
+        date: Date.parse('2012/12/01'),
+      },
+    ],
+    Talks: [
+      {
+        title: 'Domestic Motion Capture',
+        location: 'Cadiz GameDev MeetUp',
+        date: Date.parse('2023/12/15'),
+      },
+    ],
+  };
+}
