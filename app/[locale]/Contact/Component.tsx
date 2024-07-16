ebe1f8865a7a8ccd732d0b450067f485fa065445
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2024-07-16 10:59:32 +0200

    Implemented Contact Me Page

---
+++ app/[locale]/Contact/Component.tsx
@@ -0,0 +1,33 @@
+'use client';
+/* eslint-disable n/no-extraneous-import */
+import {ReactNode, useEffect, useState} from 'react';
+import {GetSocials, social} from '@/src/data/arrayRRSS';
+import {useTranslations} from 'next-intl';
+
+export function ContactMe(): ReactNode {
+  const t = useTranslations('Contact');
+  const [decryptedSNS, setDecryptedSocial] = useState<
+    Array<social> | undefined
+  >(undefined);
+  useEffect(() => {
+    GetSocials().then(socials => setDecryptedSocial(socials));
+  }, []);
+  if (decryptedSNS === undefined) {
+    return <></>;
+  }
+  return (
+    <div className="flex flex-col items-center align-middle">
+      <p>{t('Heading')}</p>
+      <ul className="pt-5 grid space-y-2 items-center">
+        {decryptedSNS.map(e => (
+          <li key={e.name + 'Contact'}>
+            <a className="flex align-middle" href={e.url}>
+              {e.logo} <p className="pl-5">{e.name}</p>{' '}
+              {e.innerText !== undefined ? ': ' + e.innerText : ''}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
