e10f47d0c51bee4be4c9fb759431480b5228e51c
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2024-11-25 18:16:58 +0100

    Fixed Dropdown NavBar in Mobile mode

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -3,7 +3,6 @@ import {Link, usePathname, useRouter} from './navigation';
 import {Dropdown, Navbar} from 'flowbite-react';
 import {useTranslations} from 'next-intl';
 import {ReactNode} from 'react';
-
 export const locales = ['en', 'es'] as const;
 export const localePrefix = 'always'; // Default
 
@@ -18,7 +17,7 @@ export function NavBar(props: {localeSwitcher: ReactNode}) {
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
-      <Navbar.Collapse>
+      <Navbar.Collapse className="flex w-full items-center justify-between">
         <Navbar.Link as={Link} href="/" active={usePathname() === '/'}>
           Home
         </Navbar.Link>
@@ -45,9 +44,12 @@ export function NavBar(props: {localeSwitcher: ReactNode}) {
           {t('Contact')}
         </Navbar.Link>
         <Dropdown
+          theme={{inlineWrapper: 'px-3 py-1 md:p-0 flex items-center'}}
           arrowIcon={true}
           inline
-          label={<Navbar.Link>{t('Others')}</Navbar.Link>}
+          label={
+            <p className="hover:text-cyan-700 text-gray-700">{t('Others')}</p>
+          }
         >
           <Dropdown.Item
             onClick={() => {
