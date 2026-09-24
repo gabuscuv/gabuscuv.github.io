f268aec7177dd60d5c4b9f0d526c9c2600545dd6
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2026-09-24 17:08:11 +0200

    Improved long text maintainability.

--- app/[locale]/AboutMe/components/Sinopsis.tsx
+++ app/[locale]/AboutMe/components/Sinopsis.tsx
@@ -2,19 +2,18 @@ import {getTranslations} from 'next-intl/server';
 import {ReactNode} from 'react';
 
 export async function Sinopsis(): Promise<Awaited<ReactNode>> {
-  const t = await getTranslations('AboutMe.Paragraphs');
+
+  const t = await getTranslations('AboutMe');
+  const paragraphs = t.raw('Paragraphs') as string[];  // const t = await getTranslations('AboutMe.Paragraphs');
   // const t = useTranslations('AboutMe.Paragraphs');
 
   return (
-    <>
-      <div className="space-y-2">
-        <p>{t('Paragraph1')}</p>
-        <p>{t('Paragraph2')}</p>
-        <p>{t('Paragraph3')}</p>
-        <p>{t('Paragraph4')}</p>
-        <p>{t('Paragraph5')}</p>
-        <p>{t('Paragraph6')}</p>
-      </div>
-    </>
-  );
+    <div>
+      {paragraphs.map((paragraph, index) => (
+        <p key={index}>
+          {paragraph}
+        </p>
+      ))}
+    </div>
+  )
 }
